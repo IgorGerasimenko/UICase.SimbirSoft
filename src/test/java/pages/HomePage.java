61564c2ad11270e806d8646636f2e3e01f2a5dfd
User <planetz@mail.ru>
2021-04-22 17:08:42 +0400

    Код после отработки замечаний №2

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -5,64 +5,56 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.Set;
 
-public class HomePage {
-    private WebDriver driver;
+public class HomePage extends Page {
 
     public HomePage(WebDriver driver) {
-        PageFactory.initElements(driver,this);
+        super(driver);
+        PageFactory.initElements(this.driver, this);
         this.driver = driver;
     }
 
-    @FindBy(xpath="//div[@class='desk-notif-card__login-new-item-title']")
+    @FindBy(xpath = "//div[@class='desk-notif-card__login-new-item-title']")
     public WebElement signInButton;
 
-    @FindBy(xpath="//div[@class='desk-notif-card__mail-title']")
+    @FindBy(xpath = "//div[@class='desk-notif-card__mail-title']")
     public WebElement mailButton;
 
 
-    public HomePage open(){
+    public HomePage open() {
         driver.get("https://yandex.ru/");
         driver.manage().window().maximize();
         return this;
     }
 
-    public HomePage clickSignIn(){
-       signInButton.click();
-       return this;
+    public HomePage clickSignIn() {
+        signInButton.click();
+        return this;
     }
 
-    public void openMailPage(){
-
+    public void openMailPage() {
+        // получаем набор дескрипторов текущих открытых окон
         String originalWindow = driver.getWindowHandle();
         final Set<String> oldWindowsSet = driver.getWindowHandles();
 
         mailButton.click();
 
-
-        String newWindowHandle = (new WebDriverWait(driver, 10))
-                .until(new ExpectedCondition<String>() {
-                           public String apply(WebDriver driver) {
-                               Set<String> newWindowsSet = driver.getWindowHandles();
-                               newWindowsSet.removeAll(oldWindowsSet);
-                               return newWindowsSet.size() > 0 ?
-                                       newWindowsSet.iterator().next() : null;
-                           }
-                       }
-                );
-
+        // ожидаем открытия и получаем дескриптор нового окна
+        String newWindowHandle = wait.until(new ExpectedCondition<String>() {
+                                                public String apply(WebDriver driver) {
+                                                    Set<String> newWindowsSet = driver.getWindowHandles();
+                                                    newWindowsSet.removeAll(oldWindowsSet);
+                                                    return newWindowsSet.size() > 0 ?
+                                                            newWindowsSet.iterator().next() : null;
+                                                }
+                                            }
+        );
+        // переключаемся на новое окно
         driver.switchTo().window(newWindowHandle);
+
 
     }
 
 
-
-
-
-
 }
-
-
