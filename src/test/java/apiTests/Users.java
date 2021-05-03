c68a8f2aa1d2094fbb663b2811a3d8d9f8a64337
IgorGerasimenko <58310275+IgorGerasimenko@users.noreply.github.com>
2021-05-03 19:08:36 +0400

    Решение задаче через циклы, не самый лучший вариант, но оставлю на память

--- src/test/java/apiTests/Users.java
+++ src/test/java/apiTests/Users.java
@@ -1,61 +1,92 @@
 package apiTests;
 
-import apiTests.pojos.UserPojo;
+import apiTests.pojos.User;
+import com.sun.istack.NotNull;
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import org.junit.Test;
 import java.util.List;
+
 import static io.restassured.RestAssured.given;
-import static org.junit.Assert.assertNotNull;
+import static io.restassured.path.json.JsonPath.with;
+import static org.junit.Assert.*;
 
 public class Users {
+    String pageCount;
+    int pageNumber;
+    List<User> users;
+    User result;
 
-    @Test
-    public void checkGeorgBluthEmailPattern (){
-        List<UserPojo> users = given()
-                .baseUri("https://reqres.in/api")
-                .basePath("/users")
+
+
+    public void getPageCount() {
+        JsonPath jspath = given()
+                .when()
+                .get("https://reqres.in/api/users?page=1")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .jsonPath();
+
+        pageCount = jspath.getString("total_pages");
+        System.out.println("method 'users' have a " + pageCount + " pages");
+    }
+
+    public void findUser (String firstName, String lastName) {
+        users = given()
                 .contentType(ContentType.JSON)
-                .when().get()
+                .when()
+                .get("https://reqres.in/api/users?page=" + pageNumber)
                 .then()
                 .statusCode(200)
-                .extract().jsonPath().getList("data", UserPojo.class);
-                UserPojo result = users.stream()
-                .filter(p -> {
-                    if ("george.bluth@reqres.in".equals(p.getEmail()) &
-                            "George".equals(p.getFirst_Name()) &
-                            "Bluth".equals(p.getLast_Name()))
-                    {
-                        return true;
-                    }
-                        return false;
-                }).findAny()
+                .extract().jsonPath().getList("data", User.class);
+
+        result = users.stream()
+                .filter(p ->
+                        firstName.equals(p.getFirstName()) &
+                                lastName.equals(p.getLastName()))
+                .findAny()
                 .orElse(null);
-         assertNotNull(result);
+    }
+
+
+    @Test
+    public void checkGeorgBluthEmailPattern(){
+        getPageCount();
+        int pageCnt = Integer.parseInt(pageCount);
+        for(pageNumber = 1;pageNumber <= pageCnt; pageNumber++) {
+            findUser("George", "Bluth");
+
+            if(result == null) {
+                pageNumber++;
+                System.out.println("На этой странице не найдено такого пользователя, поищу на следующей");
+            } else  {
+                System.out.println("Пользователь найден, проверяю его email");
+                assertEquals(result.getEmail(),"george.bluth@reqres.in" );
+                break;
+            }
+        }
     }
 
     @Test
-    public void checkMichaelLawsonEmailPattern  (){
-        List<UserPojo> users = given()
-                .baseUri("https://reqres.in/api")
-                .basePath("/users")
-                .contentType(ContentType.JSON)
-                .when().get()
-                .then()
-                .statusCode(200)
-                .extract().jsonPath().getList("data", UserPojo.class);
-        UserPojo result = users.stream()
-                .filter(p -> {
-                    if ("michael.lawson@reqres.in".equals(p.getEmail()) &
-                            "Michael".equals(p.getFirst_Name()) &
-                            "Lawson".equals(p.getLast_Name()))
-                    {
-                        return true;
-                    }
-                    return false;
-                }).findAny()
-                .orElse(null);
-         assertNotNull(result);
+    public void checkMichaelLawsonEmailPattern(){
+        getPageCount();
+        int pageCnt = Integer.parseInt(pageCount);
+        for(pageNumber = 1;pageNumber <= pageCnt; pageNumber++) {
+            findUser("Michael", "Lawson1");
+
+            if(result == null) {
+                System.out.println("На этой странице не найдено такого пользователя, поищу на следующей");
+                continue;
+
+            }else  {
+                System.out.println("Пользователь найден на странице " + pageNumber + ",проверяю его email");
+                assertEquals(result.getEmail(),"michael.lawson@reqres.in" );
+                break;
+            }
+        }
     }
 }
-
 
---
+++ src/test/java/apiTests/pojos/User.java
@@ -0,0 +1,37 @@
+package apiTests.pojos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+@Getter
+@AllArgsConstructor
+@ToString
+@NoArgsConstructor
+public class User {
+
+    private int id;
+    private String email;
+    @JsonProperty("first_name")
+    private String firstName;
+    @JsonProperty("last_name")
+    private String lastName;
+    private String avatar;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+}
